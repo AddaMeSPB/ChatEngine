4d212dd944cf3dde542c839078e60246bde72d9f
saroar <saroar9@gmail.com>
2020-11-17 12:11:47 +0300

    add root routes new msg

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,7 +3,7 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return "It works!"
+        return "ChatEngine still working!"
     }
 
     app.get("hello") { req -> String in
